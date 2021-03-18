5a5cb56bd77b572d870ba4defbb6faae6cec3748
bogdan159tbi <bogdanmica36@gmail.com>
2021-03-18 11:26:01 +0200

    string & types in haskell

--- lab/lab2/lab2.hs
+++ lab/lab2/lab2.hs
@@ -1,7 +1,8 @@
 -- 21. Extract the fourth element from a list of integers 
 {-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 -- cum fac daca lista are mai putin de 4 elemente ?
-
+import Data.Char
+import Data.List
 checkNoElem :: [Integer] -> Integer
 checkNoElem [] = 0
 checkNoElem ( element: list) = 1 + checkNoElem list
@@ -95,19 +96,30 @@ checkPositive list = (checkGreater.getElem) list
 
 -- s a schimbat notarea ex la lab 2 aparent :-?
 -- 2.1.1. Write a function which returns the number of elements from a list. Use patterns. 
+noElem :: [a] -> Integer
 noElem [] = 0
 noElem (elem : list) = 1 + noElem(list)
 
 -- 2.1.2 concatenate lists
-
-concatList [[]] = []
-concatList[list1] = list1
-concatList (sublist:list) = sublist++concatList(list)
+concatList :: [[a]] -> [a]
+concatList [] = []
+concatList (sublist:list) = sublist ++ (concatList list)
 
 -- 2.1.3 same as 2.1.2 this time using only the cons (:) operator as well as patterns. 
-
+concat2 :: [[a]] -> [a]
+concat2 [] = []
+concat2 ([] : list) = concat2 list
+concat2 ( (elem : sublist) : list) = elem : (concat2 (sublist : list))
+--func care elimina nr egale cu 2 din lista
+remove2 :: [Integer ] -> [Integer ]
+remove2 [] = []
+remove2 (elem : list) 
+                  | (elem == 2) = (remove2 list)
+                  | otherwise = elem : (remove2 list)
 -- 2.1.4. Write a function which removes duplicates from a list of integers. 
 {--
+functie care verifica daca exista elem 
+si fac cu guards
 --}
 
 checkElem elem [] = False 
@@ -130,3 +142,70 @@ removeDuplicate list  = remove  (insertSort(list))
                                                 |  otherwise  = remove list
 
                                     
+-- 2.3.1  2.3.1. Write a function which takes a list of words and makes the first letter of each word uppercase. 
+toUpperFirst :: [[Char]] -> [ [Char] ]
+toUpperFirst [] = []
+toUpperFirst ( (letter: word) : words) = (( toUpper letter):word): (toUpperFirst words)
+
+-- SAU
+
+convertChar :: [String] -> [String]
+convertChar [] = []
+convertChar (word : words) = (makeUpper word) : (convertChar words)
+                              where makeUpper [] = []
+                                    makeUpper (letter : x) = (toUpper letter):x
+
+
+-- 2.3.3 (!) Write a function which takes a text and a pattern and returns the number of occurrences of the pattern in the text. Example: 
+-----         pattern   sir de cautat este prefix sau nu ?
+
+-- ia prefix pentru ca verificam caracter cu caracter
+-- daca pattern ul se regaseste in fiecare sub cuvant
+matchFirst :: String -> String -> Bool 
+matchFirst [] str = True
+matchFirst _ [] = False
+matchFirst (letter : word) (letter2 : str) = (letter == letter2)  && (matchFirst word str)
+
+--        sir       pattern   nr aparitii
+search :: String -> String -> Integer 
+search [] pattern = 0
+search (letter: stream ) pattern 
+                              | (matchFirst pattern (letter : stream)) == True = 1 + (search stream pattern)
+                              | otherwise  = (search stream pattern)
+-- 2.3.4 find students in certain group whose names start with M
+-- nu stiu sigur daca e ok sa scriu mai multe functii in modul asta
+startsWithM :: [Char] -> Bool 
+startsWithM [] = False 
+startsWithM (letter : word) 
+                        | (letter == 'M') = True
+                        | otherwise = False 
+
+foundGroup :: [Char] -> [Char] -> Bool 
+foundGroup [] [] = False 
+foundGroup group gr 
+                  | (group == gr) = True
+                  | otherwise  = False
+getNames :: [String] -> [String]
+getNames [] = []
+getNames(name:names) 
+                | (startsWithM name) = name: (getNames names)
+                | otherwise = (getNames names)
+findKidM :: String -> [ (String,[String]) ] -> [String]
+findKidM group [] = []
+findKidM group ((grp,names):serie)
+               | (foundGroup group grp) =  getNames(names)
+               | otherwise = (findKidM group serie)
+
+
+-- 2.3.5
+-- f["Matei", "Mihai"] ["Popovici","Dumitru"] = [("Matei","Popovici"), ("Mihai","Dumitru")]
+
+matchNames :: [String] -> [String] -> [(String,String)]
+matchNames [] [] = []
+matchNames (sur : listSur) (name: listNames) = (sur,name): (matchNames listSur listNames)
+
+-- 2.3.6
+--   f ["Matei", "Mihai"] ["Popovici","Dumitru"] = ["MPopovici", "MDumitru"]
+matchNames2 :: [String] -> [String] -> [String]
+matchNames2 [] [] = []
+matchNames2 ((letter:sur) : listSur) (name: listNames) = (letter:name): (matchNames2 listSur listNames)
